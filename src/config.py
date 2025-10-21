e91a653204d11fe1b32b73713da00e27b45e4b61
okar0 <oskar.dolega@gmail.com>
2025-10-20 20:30:09 -0400

    Add load_common to parse Common.cfg into CommonCfg

--- src/config.py
+++ src/config.py
@@ -3,13 +3,29 @@ from pathlib import Path
 
 @dataclass
 class CommonCfg:
-    NumberofPreferedNeighbors: int
+    NumberofPreferredNeighbors: int
     UnchokingInterval: int
     OptimisticUnchokingInterval: int
     FileName: str
-    FileSize: str
+    FileSize: int
     PieceSize: int
 
 def _kv(line: str):
     k, v = line.strip().split(maxsplit=1)
     return k, v
+
+def load_common(path: str | Path) -> CommonCfg:
+    d ={}
+    with open(path, "r", encoding = "utf-8") as f:
+        for line in f:
+            if not line.strip(): continue
+            k, v = _kv(line)
+            d[k] = v
+    return CommonCfg(
+        NumberofPreferedNeighbors=int(d["NumberofPreferredNeighbors"]), 
+        UnchokingInterval=int(d["UnchokingInterval"]),
+        OptimisticUnchokingInterval=int(d["OptimisticUnchokingInterval"]),
+            FileName=d["FileName"],
+            FileSize=int(d["FileSize"]),
+            PieceSize=int(d["PieceSize"]),
+    )
